477500fb6f291256e7c2b8a9b926fa145b2966fd
frunox <60527588+frunox@users.noreply.github.com>
2020-04-22 21:39:58 -0500

    updated projects, links, resume, backgrounds

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $("body").floatingSocialShare({
+        counter: false,
+        buttons: ["facebook", "twitter", "linkedin", "mail"],
+        text: {
+            "default": "share with:",
+            "facebook": "share with facebook",
+            "twitter": "tweet"
+        },
+
+    });
+
+});
