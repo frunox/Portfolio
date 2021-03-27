e6d2696895dcf0f638d1e14b553891c0ffe871a0
frunox <60527588+frunox@users.noreply.github.com>
2021-03-26 19:35:35 -0500

    revised HomeNav

--- client/src/components/HomeNav/index.js
+++ client/src/components/HomeNav/index.js
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
 import DevDataContext from "../../contexts/DevDataContext";
 // import SetupContext from "../../contexts/SetupContext";
-import { Menu } from 'semantic-ui-react';
 import "./HomeNav.css";
 
 const HomeNav = () => {
   const devCtx = useContext(DevDataContext);
+  let userName = `${devCtx.state.fname} ${devCtx.state.lname}`
   // const setupCtx = useContext(SetupContext);
   // console.log('HOMENAV some devData: ', devCtx.state.fname)
   // console.log('HOMENAV setupCtx', setupCtx);
@@ -14,28 +14,21 @@ const HomeNav = () => {
   //   loggedIn = true
   // }
   let content = (
-    <div>
-      <Menu inverted stackable fixed="top" className="menu">
-        <Menu.Item header className="logo">{devCtx.state.fname} {devCtx.state.lname}</Menu.Item>
-        <Menu.Menu position="left">
-          <Menu.Item as="a" href="/" name="home">
-          </Menu.Item>
-        </Menu.Menu>
-        <Menu.Menu position="right">
-          <Menu.Item as="a" href="/about" name="about">
-          </Menu.Item>
-
-          <Menu.Item as="a" href="/contact" name="contact">
-          </Menu.Item>
-
-
-          <Menu.Item as="a" href="/developer" name="developer">
-          </Menu.Item>
-
-        </Menu.Menu>
-      </Menu>
-
-    </div >
+    <header className='homenav-header'>
+      <h1 className="user-name">{userName}</h1>
+      <input type='checkbox' id="navbar-toggle" className="navbar-toggle"></input>
+      <nav className='navbar-nav'>
+        <ul className='navbar-ul'>
+          <li className='navbar-li>'><a href='/'>Home</a></li>
+          <li className='navbar-li>'><a href='/about'>About</a></li>
+          <li className='navbar-li>'><a href='/contact'>Contact</a></li>
+          <li className='navbar-li>'><a href='/developer'>Developer</a></li>
+        </ul>
+      </nav>
+      <label for='navbar-toggle' className='navbar-toggle-label'>
+        <span></span>
+      </label>
+    </header >
   )
   return content
 }
