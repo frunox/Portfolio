11771a135a604aafd935c7039aeb2a8c6f09fa3b
frunox <60527588+frunox@users.noreply.github.com>
2021-03-31 12:08:31 -0500

    Re-write instructions, adjust vertical spacing

--- client/src/components/DevContainer/index.js
+++ client/src/components/DevContainer/index.js
@@ -8,9 +8,12 @@ const DevContainer = () => (
     <Fragment>
         <div className='devContainer'>
             <ul className="bList">
-                <li>Review your projects in the table below</li>
+                <li>Log in to make changes</li>
+                <li>Sort projects by table headings or search using keywords</li>
                 <li>Projects with Show = true will display on the home page</li>
-                <li>Click on a project row to change the Display status or add an image link and search keywords</li>
+                <li>Click on a project row to change the Show status, add an image link and/or search keywords</li>
+                <li>Change user settings (name, email, resume/LinkedIn links) via the User link</li>
+                <li>Use Sync to re-load your GitHub repos to update changes (Show status, image links and search keywords will reset)</li>
             </ul>
             <hr />
         </div>
